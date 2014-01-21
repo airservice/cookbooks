bdd6cbad27268c131c410458596c24df05488c1b
Maxim Filimonov <tpaktopsp@gmail.com>
2014-01-21 11:18:24 +1100

    trying different syntax

--- env/recipes/default.rb
+++ env/recipes/default.rb
@@ -1,9 +1,7 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  log "Deploying to #{deploy[:current_path]}" do
-    level :info
-  end
+  Chef::Log.info "Deploying to #{deploy[:current_path]}"
   execute "output test file" do
     cwd deploy[:current_path]
     command 'echo 123 > test.file'
