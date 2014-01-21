127b3d8752ac1decda74dd363cabdf4f91b9a020
Maxim Filimonov <tpaktopsp@gmail.com>
2014-01-21 11:07:16 +1100

    adding log to deployment

--- env/recipes/default.rb
+++ env/recipes/default.rb
@@ -1,6 +1,7 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
+  puts "DEPLOYING TO #{deploy[:current_path]}"
   execute "output test file" do
     cwd deploy[:current_path]
     command 'echo 123 > test.file'
